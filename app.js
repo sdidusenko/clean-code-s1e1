6b2ebe6b319df6e637088be1be23814bb6bf906a
Serhii Didusenko <didusenko40@gmail.com>
2023-04-21 01:45:56 +0300

    fix(bacic-3.2&3.3&3.4&3.8): add important classes name, brevity names, remove tag selectors

--- app.js
+++ app.js
@@ -34,18 +34,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className = 'task';
+    listItem.className = 'list'
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    // editInput.className="task";
+    editInput.classList.add("task", "input")
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.classList.add("button", "edit");
 
     deleteButton.classList.add("button", "delete");
-    deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.src = './remove.svg';
+    deleteButtonImg.classList.add("delete-ico")
     deleteButton.appendChild(deleteButtonImg);
 
 
